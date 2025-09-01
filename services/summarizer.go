b55cf198449f0746093fa62d724127200d1acaa8
vansh-commits <vansh.malhotra439@hotmail.com>
2025-09-01 16:58:36 +0530

    implemented gemini endpointed

--- services/summarizer.go
+++ services/summarizer.go
@@ -35,7 +35,7 @@ func Summarizer(text string) (string, error) {
 	}
 
 	url := fmt.Sprintf(
-		"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=%s",
+		"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=%s",
 		apiKey,
 	)
 
